914aca215c0b1397b2e3236fe882b594557c5e49
Sirawit Pratoomsuwan <sirawit.p@connectedtech.co.th>
2025-02-25 20:12:23 +0700

    fix: remove error log

--- value_ptr.go
+++ value_ptr.go
@@ -1,8 +1,6 @@
 package gut
 
 import (
-	"log"
-
 	"github.com/google/uuid"
 )
 
@@ -11,7 +9,7 @@ func Ptr[T any](v T) *T {
 }
 
 // Uint8Ptr converts a UUID string or *string to *[]uint8, with an optional default value.
-func Uint8Ptr[T string | *string](id T, defaultValue ...string) (*[]uint8, error) {
+func Uint8Ptr[T string | *string](id T) *[]uint8 {
 	var strID string
 
 	// Handle input types (string or *string)
@@ -20,29 +18,17 @@ func Uint8Ptr[T string | *string](id T, defaultValue ...string) (*[]uint8, error
 		strID = v
 	case *string:
 		if v == nil {
-			return nil, nil // Safe handling for nil pointer
+			return nil // Safe handling for nil pointer
 		}
 		strID = *v
 	default:
-		return nil, nil // Invalid type
+		return nil // Invalid type
 	}
 
-	// Try parsing the UUID
 	uuidBytes, err := uuid.Parse(strID)
 	if err != nil {
-		log.Printf("Invalid UUID: %v", err) // Log error
-
-		// If a default value is provided, use it
-		if len(defaultValue) > 0 {
-			uuidBytes, err = uuid.Parse(defaultValue[0])
-			if err != nil {
-				return nil, err // Even the default is invalid, return error
-			}
-		} else {
-			return nil, err // No default, return error
-		}
+		return nil // Invalid UUID
 	}
-
 	bytes := uuidBytes[:]
-	return &bytes, nil
+	return &bytes
 }
