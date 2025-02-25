b3e5b92a5843d54d446003344be8460c3b4761f6
Sirawit Pratoomsuwan <sirawit.p@connectedtech.co.th>
2025-02-25 19:56:51 +0700

    feat: add unit8ptr

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/google/uuid v1.6.0
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/crypto v0.27.0 // indirect
--- value_ptr.go
+++ value_ptr.go
@@ -1,5 +1,48 @@
 package gut
 
+import (
+	"log"
+
+	"github.com/google/uuid"
+)
+
 func Ptr[T any](v T) *T {
 	return &v
+}
+
+// Uint8Ptr converts a UUID string or *string to *[]uint8, with an optional default value.
+func Uint8Ptr[T string | *string](id T, defaultValue ...string) (*[]uint8, error) {
+	var strID string
+
+	// Handle input types (string or *string)
+	switch v := any(id).(type) {
+	case string:
+		strID = v
+	case *string:
+		if v == nil {
+			return nil, nil // Safe handling for nil pointer
+		}
+		strID = *v
+	default:
+		return nil, nil // Invalid type
+	}
+
+	// Try parsing the UUID
+	uuidBytes, err := uuid.Parse(strID)
+	if err != nil {
+		log.Printf("Invalid UUID: %v", err) // Log error
+
+		// If a default value is provided, use it
+		if len(defaultValue) > 0 {
+			uuidBytes, err = uuid.Parse(defaultValue[0])
+			if err != nil {
+				return nil, err // Even the default is invalid, return error
+			}
+		} else {
+			return nil, err // No default, return error
+		}
+	}
+
+	bytes := uuidBytes[:]
+	return &bytes, nil
 }
